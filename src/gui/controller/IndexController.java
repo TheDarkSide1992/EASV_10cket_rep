eb01197e326e20a94c14cdda6d47333779d45fac
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-17 11:49:46 +0100

    ControllerAssistant and singleton

--- src/gui/controller/IndexController.java
+++ src/gui/controller/IndexController.java
@@ -1,13 +1,24 @@
 package gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IndexController implements Initializable {
+    @FXML private BorderPane borderPane;
+
+    private ControllerAssistant controllerAssistant;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        controllerAssistant = ControllerAssistant.getInstance();
+        controllerAssistant.setBorderPane(borderPane);
     }
+
+
 }
