830fa784bb3fe96ed6eb513fb753b53ccc978937
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-20 16:38:01 +0100

    SQl script V2 : Now with Event tabele and updated Tickets

--- data/SQL/CreateDB.sql
+++ data/SQL/CreateDB.sql
@@ -54,11 +54,36 @@ CREATE TABLE Customer(
 )
 GO
 
+CREATE TABLE Event_(
+    Event_ID                    INT IDENTITY(1,1)                       NOT NULL,
+    Event_TIttle                NVARCHAR(250)                           NOT NULL,
+    Event_Location              NVARCHAR(250)                           NOT NULL,
+    Event_Event_Cordinator_ID   INT                                     NOT NULL,  --FK 2 cordinator
+    Evnet_Authors               NVARCHAR(250)                           NOT NULL,
+    Event_Date                  DATE                                    NOT NULL,
+    Event_Start_Time            TIME                                    NOT NULL,
+    Event_Description           NVARCHAR(750),
+    Event_Ticket_Total          INT                                     NOT NULL,
+    Event_Tickett_Sold          INT                                     NOT NULL,
+    Event_Is_Actrtive           BIT                                     NOT NULL,
+    Event_Img_                  VARBINARY(MAX),
+
+    CONSTRAINT PK_EVENT_ID PRIMARY KEY(Event_ID),
+
+    CONSTRAINT FK_EVENT_EVENT_CORDINATOR_ID FOREIGN KEY(Event_Event_Cordinator_ID)
+    REFERENCES User_(User_ID)
+)
+GO
+
 CREATE TABLE Ticket_(
     Ticket_Content_ID           INT IDENTITY(1,1)                       NOT NULL,
-    Ticket_Content_Contains     NVARCHAR(250)                           NOT NULL,
+    Ticket_Event_ID             INT                                     NOT NULL, --FK 2 Event_
+    Ticket_Contains             NVARCHAR(100)                           NOT NULL,
 
-    CONSTRAINT PK_TICKET_CONTENT_ID PRIMARY KEY(Ticket_Content_ID)
+    CONSTRAINT PK_TICKET_CONTENT_ID PRIMARY KEY(Ticket_Content_ID),
+
+    CONSTRAINT FK_TICKET_EVENT_ID FOREIGN KEY(Ticket_Event_ID)
+    REFERENCES Event_(Event_ID)
 )
 GO
 
