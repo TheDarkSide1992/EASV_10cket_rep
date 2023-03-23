fd67dee2174ae4e7926ee6c1fbff1297e302cce4
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-03-23 15:56:57 +0100

    Sign in effect added

--- src/gui/controller/CustomerTopViewController.java
+++ src/gui/controller/CustomerTopViewController.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,6 +19,7 @@ import java.util.ResourceBundle;
 
 public class CustomerTopViewController implements Initializable {
 
+    public Label lblSignIn;
     private String url = "/data/Images/10cketshort";
 
     @FXML
@@ -26,7 +30,15 @@ public class CustomerTopViewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        //setLogo();
+        signInLabelStyling();
 
+
+    }
+
+    private void signInLabelStyling() {
+        DropShadow shadow = new DropShadow(0,4,4, Color.color(0,0,0,0.25));
+        lblSignIn.setEffect(shadow);
+        lblSignIn.getStyleClass().add("lblSignIn");
     }
 
     private void setLogo() {
