7e7f5bafafac00ac15cf0687b9e806a29f8483bf
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-24 13:09:41 +0100

    pasword check

--- src/be/EventCoordinator.java
+++ src/be/EventCoordinator.java
@@ -4,27 +4,4 @@ public class EventCoordinator extends User {
     public EventCoordinator(int userID, String userNAme, String userFirstName, String userEmail, String userTLF, int userTypeId) {
         super(userID, userNAme, userFirstName, userEmail, userTLF, userTypeId);
     }
-
-
-
-    /*
-    public boolean validPassword(String password){
-        String specialChars = "!,.:;<>\\/()#%=+?'*";
-        if (password.length()>8){
-            for (int i = 0; i < password.length(); i++) {
-                for (int j = 0; j< specialChars.length(); i++){
-                    if(password.charAt(i) == specialChars.charAt(j)){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    public void setPassword(String password){
-        if (!validPassword(password)){
-            this.password = password;
-        }
-    } */
-
 }
