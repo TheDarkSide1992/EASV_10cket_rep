e2c6cb79213dcb005ba1495a7f41b814325b911f
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-27 12:31:18 +0200

    minor sql update, with new table

--- data/SQL/InsertMoxk.sql
+++ data/SQL/InsertMoxk.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO User_Type VALUES ('Administrator');
 INSERT INTO User_Type VALUES ('Event Coordinator');
 GO
@@ -5,16 +6,16 @@ GO
 INSERT INTO User_ VALUES('Def_EV_Cord', 'DEFAULT', (SELECT DISTINCT User_Type_ID FROM User_Type WHERE USER_TYPE_TYPE = 'Event Coordinator'), 'N/A@EASV.dk', 'TLF = N/A')
 GO
 
-INSERT INTO Event(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
+INSERT INTO Event_(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
 VALUES('Open Mic Night', 'THE BASEMENT', 1, 'N/A',  convert(date, '2023-5-15'), convert(time, '18:00:00'), 'N/A', 100, 0, 1, CAST('N/A' AS VARBINARY(MAX)))
 
-INSERT INTO Event(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
+INSERT INTO Event_(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
 VALUES('Game Night', 'THE BASEMENT', 1, 'N/A',  convert(date, '2023-4-05'), convert(time, '17:30:00'), 'N/A', 100, 0, 1, CAST('N/A' AS VARBINARY(MAX)))
 
-INSERT INTO Event(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
+INSERT INTO Event_(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
 VALUES('Singer/Songwriter Jesus', 'THE BASEMENT', 1, 'N/A',  convert(date, '2023-7-25'), convert(time, '19:00:00'), 'N/A', 100, 0, 1, CAST('N/A' AS VARBINARY(MAX)))
 
-INSERT INTO Event(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
+INSERT INTO Event_(Event_Title, Event_Location, Event_Event_Coordinator_ID, Event_Authors, Event_Date, Event_Start_Time, Event_Description, Event_Ticket_Total, Event_Ticket_Sold, Event_Is_Active, Event_Img)
 VALUES('INTRO PARTY', 'THE BASEMENT', 1, 'N/A',  convert(date, '2023-9-18'), convert(time, '17:45:00'), 'N/A', 100, 0, 1, CAST('N/A' AS VARBINARY(MAX)))
 GO
 
@@ -24,5 +25,5 @@ GO
 SELECT * FROM User_ WHERE User_Type = (SELECT User_Type_ID FROM User_Type WHERE USER_TYPE_TYPE = 'Event Coordinator')
 GO
 
-SELECT * FROM Event
+SELECT * FROM Event_
 GO
