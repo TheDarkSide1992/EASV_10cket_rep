ed175ccb96fda1ebffb6d8855409b76ba80985d8
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-30 12:25:45 +0200

    sql update

--- data/SQL/CreateDB.sql
+++ data/SQL/CreateDB.sql
@@ -25,6 +25,7 @@ CREATE TABLE User_(
     User_Type                   INT                                     NOT NULL,
     User_Email                  NVARCHAR(250)                           NOT NULL,
     User_tlf                    NVARCHAR(50)                            NOT NULL,
+    User_Img                    VARBINARY(MAX),
 
     CONSTRAINT PK_USER_ID PRIMARY KEY(User_ID),
 
