66449da37ae069d5a8b26e2e1110bd4a4e75cbe5
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-04-13 10:50:27 +0200

    ManageTickets

--- src/gui/controller/ManageTicketsController.java
+++ src/gui/controller/ManageTicketsController.java
@@ -1,4 +1,30 @@
 package gui.controller;
 
-public class ManageTicketsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageTicketsController implements Initializable {
+    @FXML
+    private Button btnAdd, btnSaveTickets;
+    @FXML
+    private ChoiceBox comboChooseEvent, comboTypeOfTicket;
+    @FXML private TextField txtTitleOfEvent, txtDateOfEvent, txtTotalAmountOfTickets, txtTicketsSold, txtLocationOfEvent, txtAddExtras, txtNumberOfTickets ,txtNewPriceOfTicket;
+    @FXML private TableView tblviewTypesOfTickets;
+    @FXML private TableColumn colType,colContains, colPrice, colAmount;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleAdd(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveTickets(ActionEvent actionEvent) {
+    }
 }
